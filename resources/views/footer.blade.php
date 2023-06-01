90d6f3c7803b100754e03fddc48308ebfd0c48df
Muhammad Zeeshan <shanii6057@gmail.com>
2023-06-01 17:26:54 +0500

    loging page  done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,51 @@
+<div>
+    <div style="padding-top:50px; padding-bottom: 20px" class="row navbar-dark bg-primary"  >
+        <div class="col col-sm-5"style="color:#fff;" >
+        <div class="container">
+            <h1>Ecom Project</h1>
+            <p>Introducing our e-commerce store, where style meets convenience. Explore a world of trendy fashion and accessories curated just for you. From statement pieces to everyday essentials, we've got everything you need to elevate your wardrobe effortlessly.</p>
+            </div>
+        </div>
+        <div class="col" style="color:#fff;">
+            <h3>Pages</h3>
+        <ul class="list-group">
+         <li >Cras justo odio</li>
+          <li >Dapibus ac facilisis in</li>
+          <li >Morbi leo risus</li>
+         <li >Porta ac consectetur ac</li>
+         <li >Vestibulum at eros</li>
+</ul>
+            
+        </div>
+        <div class="col">
+        
+
+        <form class="form-inline">
+  <label class="sr-only" for="inlineFormInputName2">Subscribe</label>
+
+
+  <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
+  <div class="input-group mb-2 mr-sm-2">
+    <div class="input-group-prepend">
+      <div class="input-group-text">@</div>
+    </div>
+    <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Email Address">
+  </div>
+
+  <div class="form-check mb-2 mr-sm-2">
+    
+  </div>
+
+  <button type="submit" class="btn btn-light mb-2">Submit</button>
+</form>
+
+            
+        </div>
+
+
+
+    </div>
+
+
+
+</div>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,32 @@
+@extends("master")
+
+@section('content')
+<div class="container"style="margin-top:100px; margin-bottom: 100px">
+<h1>Login Form</h1>
+
+    <div class="row justify-content-md-center" >
+        <div class="col-sm-4 ">
+          
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+        </div>
+         
+    </div>
+
+</div>
+@endsection()
